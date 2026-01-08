772e46d6e7f9c761f29e2841ba5e370979e7b144
junhub98 <helpwoodjs@icloud.com>
2026-01-08 20:20:31 +0900

    feat: strictMode

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,8 @@ import { createRoot } from "react-dom/client";
 import "@/shares/globalStyles/styles/global.css";
 import App from "@/app";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
